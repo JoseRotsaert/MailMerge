79163ed570ee558efea81623230c24452fc04e56
JoseRotsaert <jose.rotsaert@gmail.com>
2021-04-07 16:42:56 +0200

    cleaned up

--- main.py
+++ main.py
@@ -4,10 +4,11 @@
 #Save the letters in the folder "ReadyToSend".
     
 #Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
-    #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
-        #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+##Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
+#Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
 
-#
+PLACEHOLDER = "[name]"
+
 with open("./Input/Names/invited_names.txt", mode = "r") as input_names:
     names_list =input_names.readlines()   #readlines creates a list
     # check
@@ -17,12 +18,11 @@ with open("./Input/Letters/starting_letter.txt", mode = "r") as input_letter:
     template_letter =input_letter.read()
     # check
     # print(template_letter)
-
-for name in names_list:
-    name = name.strip("\n")
-    template_letter_name = template_letter.replace("[name]", name)
-    with open(f"./Output/ReadyToSend/starting_letter_{name}.txt", mode="w") as letter_final:
-        letter_final.write(template_letter_name)
+    for name in names_list:
+        stripped_name = name.strip("\n")
+        new_letter = template_letter.replace(PLACEHOLDER, stripped_name)
+        with open(f"./Output/ReadyToSend/starting_letter_{stripped_name}.txt", mode="w") as letter_final:
+            letter_final.write(new_letter)
 
 
 
